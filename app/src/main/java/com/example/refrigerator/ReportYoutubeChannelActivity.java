7ee094043499c3430e2b811d26c24a3f0430031f
whseol <whseol0203@naver.com>
2022-05-04 02:30:05 +0900

    ReportYoutubeChannelActivity success

---
+++ app/src/main/java/com/example/refrigerator/ReportYoutubeChannelActivity.java
@@ -0,0 +1,14 @@
+package com.example.refrigerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportYoutubeChannelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_youtube_channel);
+    }
+}
