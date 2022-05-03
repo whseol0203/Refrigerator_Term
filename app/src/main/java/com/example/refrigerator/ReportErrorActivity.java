3914be6676223521826424ce40ba41c53d1b3c79
whseol <whseol0203@naver.com>
2022-05-04 02:14:20 +0900

    ReportErrorActivity success

---
+++ app/src/main/java/com/example/refrigerator/ReportErrorActivity.java
@@ -0,0 +1,14 @@
+package com.example.refrigerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_error);
+    }
+}
